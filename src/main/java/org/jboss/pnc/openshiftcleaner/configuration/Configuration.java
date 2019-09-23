8eb17103de200c41feb999512fc8f141a703f9a2
Dustin Kut Moy Cheung <dcheung@redhat.com>
2019-09-23 16:15:37 -0400

    Remove newline for token

--- src/main/java/org/jboss/pnc/openshiftcleaner/configuration/Configuration.java
+++ src/main/java/org/jboss/pnc/openshiftcleaner/configuration/Configuration.java
@@ -7,21 +7,29 @@ import javax.enterprise.context.ApplicationScoped;
 import java.util.List;
 
 @ApplicationScoped
-@Getter
 public class Configuration {
 
+    @Getter
     @ConfigProperty(name = "openshift_server", defaultValue = "")
     String openshiftServer;
 
+    @Getter
     @ConfigProperty(name = "namespaces", defaultValue = "")
     List<String> namespaces;
 
     @ConfigProperty(name = "openshift_token", defaultValue = "")
     String token;
 
+    @Getter
     @ConfigProperty(name = "service_query", defaultValue = "pnc-ba")
     String serviceQuery;
 
+    @Getter
     @ConfigProperty(name = "route_query", defaultValue = "pnc-ba-route")
     String routeQuery;
+
+    public String getToken() {
+        // sometimes with env vars there might be a newline at the end. just remove it
+        return token.trim();
+    }
 }
